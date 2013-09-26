e4bb6533f10e8ed087d7d63d03b8b247d72c29e9
Patrick Ryan <phiat99@gmail.com>
2013-09-26 06:48:18 -0400

    remade into SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hotbits-clj "0.2.0"
+(defproject hotbits-clj "0.2.0-SNAPSHOT"
   :description "A toy clojure library for getting random bits from fourmilab.ch's Hotbits."
   :url "https://github.com/phiat/hotbits-clj"
   :license {:name "Eclipse Public License"
