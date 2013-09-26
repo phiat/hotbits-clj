f468f57b0128bf3d0acb994057813c25a61930ef
Patrick Ryan <phiat99@gmail.com>
2013-09-26 15:17:45 -0400

    rename test/hotbits dir

--- test/hotbits/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns hotbits.core-test
-  (:require [clojure.test :refer :all]
-            [hotbits.core :refer :all]))
-
-(deftest hotbits-count
-  (testing "hotbits-count-512"
-    (is (= (count (hotbits)) 512))))
